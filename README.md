733edf5eb391a5428eda4569018963f28f108292
kxue43 <xueke.kent@gmail.com>
2025-07-08 18:49:18 -0400

    feat: Lazily open `/dev/tty`.
      - So that commands other than `assume-role` works on all OS.

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 
 The `toolkit` CLI.
 
-Only works on Linux or macOS because it needs to read and write `/dev/tty`.
+The `toolkit assume-role` command only works on Linux or macOS because it needs to read and write `/dev/tty`.
+Other commands work on all operating systems.
 
 ```bash
 go install github.com/kxue43/cli-toolkit/cmd/toolkit@latest
--- cmd/toolkit/main.go
+++ cmd/toolkit/main.go
@@ -11,23 +11,51 @@ import (
 	"github.com/kxue43/cli-toolkit/scaffold"
 )
 
+type LazyTTY struct {
+	tty *os.File
+}
+
+func (l *LazyTTY) Init() {
+	tty, err := os.OpenFile("/dev/tty", os.O_RDWR|os.O_SYNC, 0600)
+	if err != nil {
+		log.Fatalf("failed to open /dev/tty: %v", err)
+	}
+
+	l.tty = tty
+}
+
+func (l *LazyTTY) Read(p []byte) (n int, err error) {
+	if l.tty == nil {
+		l.Init()
+	}
+
+	return l.tty.Read(p)
+}
+
+func (l *LazyTTY) Write(p []byte) (n int, err error) {
+	if l.tty == nil {
+		l.Init()
+	}
+
+	return l.tty.Write(p)
+}
+
+func (l *LazyTTY) Close() {
+	if l.tty != nil {
+		if err := l.tty.Close(); err != nil {
+			log.Fatalf("error closing /dev/tty: %v", err)
+		}
+	}
+}
+
 func main() {
 	var cli struct {
 		StartGoProject scaffold.GoProjectCmd `cmd:"" name:"start-go-project" help:"Start a Go project in the current directory."`
 		AssumeRole     auth.AssumeRoleCmd    `cmd:"" name:"assume-role" help:"Run AWS CLI credential process by assuming a role."`
 	}
 
-	tty, err := os.OpenFile("/dev/tty", os.O_RDWR|os.O_SYNC, 0600)
-	if err != nil {
-		log.Fatalf("failed to open /dev/tty: %v", err)
-	}
-
-	defer func() {
-		err = tty.Close()
-		if err != nil {
-			log.Fatalf("error closing /dev/tty: %v", err)
-		}
-	}()
+	tty := &LazyTTY{}
+	defer tty.Close()
 
 	ctx := kong.Parse(
 		&cli,
@@ -39,7 +67,7 @@ func main() {
 		kong.BindTo(os.Stdout, (*io.Writer)(nil)),
 	)
 
-	err = ctx.Run()
+	err := ctx.Run()
 
 	ctx.FatalIfErrorf(err)
 }
