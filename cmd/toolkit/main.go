139f28d247c663766331da9a126942dc545bd4b3
Ke Xue <xueke.kent@gmail.com>
2025-07-09 09:56:46 -0400

    feature: `start-go-project` command (#3)
    * feat: Add `start-go-project` command.
    
    * feat: Lazily open `/dev/tty`.
    
      - So that commands other than `assume-role` works on all OS.
    
    * refactor: Separate out `WriteToFile` function.
    
    * test: Add unit test for `scaffold` package.

--- cmd/toolkit/main.go
+++ cmd/toolkit/main.go
@@ -8,24 +8,54 @@ import (
 	"github.com/alecthomas/kong"
 
 	"github.com/kxue43/cli-toolkit/auth"
+	"github.com/kxue43/cli-toolkit/scaffold"
 )
 
-func main() {
-	var cli struct {
-		AssumeRole auth.AssumeRoleCmd `cmd:"" name:"assume-role" help:"Run AWS CLI credential process by assuming a role."`
-	}
+type LazyTTY struct {
+	tty *os.File
+}
 
+func (l *LazyTTY) Init() {
 	tty, err := os.OpenFile("/dev/tty", os.O_RDWR|os.O_SYNC, 0600)
 	if err != nil {
 		log.Fatalf("failed to open /dev/tty: %v", err)
 	}
 
-	defer func() {
-		err = tty.Close()
-		if err != nil {
+	l.tty = tty
+}
+
+func (l *LazyTTY) Read(p []byte) (n int, err error) {
+	if l.tty == nil {
+		l.Init()
+	}
+
+	return l.tty.Read(p)
+}
+
+func (l *LazyTTY) Write(p []byte) (n int, err error) {
+	if l.tty == nil {
+		l.Init()
+	}
+
+	return l.tty.Write(p)
+}
+
+func (l *LazyTTY) Close() {
+	if l.tty != nil {
+		if err := l.tty.Close(); err != nil {
 			log.Fatalf("error closing /dev/tty: %v", err)
 		}
-	}()
+	}
+}
+
+func main() {
+	var cli struct {
+		StartGoProject scaffold.GoProjectCmd `cmd:"" name:"start-go-project" help:"Start a Go project in the current directory."`
+		AssumeRole     auth.AssumeRoleCmd    `cmd:"" name:"assume-role" help:"Run AWS CLI credential process by assuming a role."`
+	}
+
+	tty := &LazyTTY{}
+	defer tty.Close()
 
 	ctx := kong.Parse(
 		&cli,
@@ -37,7 +67,7 @@ func main() {
 		kong.BindTo(os.Stdout, (*io.Writer)(nil)),
 	)
 
-	err = ctx.Run()
+	err := ctx.Run()
 
 	ctx.FatalIfErrorf(err)
 }
---
+++ scaffold/go_project.go
@@ -0,0 +1,112 @@
+package scaffold
+
+import (
+	_ "embed"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"text/template"
+
+	"golang.org/x/mod/modfile"
+)
+
+type (
+	GoProjectCmd struct {
+		rootDir             string
+		ModulePath          string `arg:"" required:"" name:"ModulePath" help:"Module path for the project."`
+		GoVersion           string `name:"go-version" default:"1.24.1" help:"Will appear in go.mod and GitHub Actions workflow."`
+		GolangcilintVersion string `name:"golangci-lint-version" default:"2.2.1" help:"Will appear in GitHub Actions workflow."`
+	}
+)
+
+var (
+	//go:embed "data/.golangci.yaml"
+	golangciYaml []byte
+
+	//go:embed "data/test-and-lint.yaml"
+	goTestAndLintYaml string
+)
+
+func WriteToFile(dir, name string, hook func(io.Writer) error) (err error) {
+	fd, err := os.Create(filepath.Clean(filepath.Join(dir, name)))
+	if err != nil {
+		return fmt.Errorf("failed to create %q file: %w", name, err)
+	}
+
+	err = hook(fd)
+	if err != nil {
+		return fmt.Errorf("failed to write to %q: %w", name, err)
+	}
+
+	err = fd.Close()
+	if err != nil {
+		return fmt.Errorf("failed to close %q after writing: %w", name, err)
+	}
+
+	return nil
+}
+
+func (c *GoProjectCmd) AfterApply() error {
+	var err error
+
+	c.rootDir, err = os.Getwd()
+	if err != nil {
+		return fmt.Errorf("failed to get current working directory: %w", err)
+	}
+
+	return nil
+}
+
+func (c *GoProjectCmd) Run() (err error) {
+	err = WriteToFile(c.rootDir, ".golangci.yaml", func(fd io.Writer) error {
+		_, err1 := fd.Write(golangciYaml)
+
+		return err1
+	})
+	if err != nil {
+		return err
+	}
+
+	err = os.MkdirAll(filepath.Clean(filepath.Join(c.rootDir, ".github/workflows")), 0750)
+	if err != nil {
+		return fmt.Errorf("failed to create .github/workflows directory: %w", err)
+	}
+
+	err = WriteToFile(c.rootDir, ".github/workflows/test-and-lint.yaml", func(fd io.Writer) error {
+		t, err1 := template.New("test-and-lint.yaml").Parse(goTestAndLintYaml)
+		if err1 != nil {
+			return fmt.Errorf("failed to load template for test-and-lint.yaml: %w", err1)
+		}
+
+		return t.Execute(fd, c)
+	})
+	if err != nil {
+		return err
+	}
+
+	err = WriteToFile(c.rootDir, "go.mod", func(fd io.Writer) error {
+		goModFile := new(modfile.File)
+
+		err1 := goModFile.AddModuleStmt(c.ModulePath)
+		if err != nil {
+			return fmt.Errorf("failed to format starter go.mod file: %w", err1)
+		}
+
+		err1 = goModFile.AddGoStmt(c.GoVersion)
+		if err != nil {
+			return fmt.Errorf("failed to format starter go.mod file: %w", err1)
+		}
+
+		contents, err1 := goModFile.Format()
+		if err1 != nil {
+			return fmt.Errorf("failed to format starter go.mod file: %w", err1)
+		}
+
+		_, err1 = fd.Write(contents)
+
+		return err1
+	})
+
+	return err
+}
