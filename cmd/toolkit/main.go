9e1c50bbd55fad06ce3890a3498be902fecb2514
kxue43 <xueke.kent@gmail.com>
2025-08-09 11:21:15 -0400

    feat: Add `--version` flag to `toolkit` command.

--- cmd/toolkit/main.go
+++ cmd/toolkit/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/alecthomas/kong"
 
 	"github.com/kxue43/cli-toolkit/scaffold"
+	"github.com/kxue43/cli-toolkit/version"
 )
 
 func main() {
@@ -11,14 +12,16 @@ func main() {
 		StartTsCdkProject  scaffold.TsCdkProjectCmd  `cmd:"" name:"start-ts-cdk-project" help:"Start a TypeScript CDK project in the current directory."`
 		StartPythonProject scaffold.PythonProjectCmd `cmd:"" name:"start-python-project" help:"Start a Python project in the current directory."`
 		StartGoProject     scaffold.GoProjectCmd     `cmd:"" name:"start-go-project" help:"Start a Go project in the current directory."`
+		Version            kong.VersionFlag          `name:"version" short:"v" help:"Show version information and quit."`
 	}
 
 	ctx := kong.Parse(
 		&cli,
 		kong.Name("toolkit"),
-		kong.Description("Personal CLI toolkit."),
+		kong.Description("Create starter files for Go, Python or AWS CDK projects."),
 		kong.UsageOnError(),
 		kong.ConfigureHelp(kong.HelpOptions{Compact: true}),
+		kong.Vars{"version": version.FromBuildInfo()},
 	)
 
 	err := ctx.Run()
